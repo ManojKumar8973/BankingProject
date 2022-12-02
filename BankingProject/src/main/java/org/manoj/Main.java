1632959367f551eaaaeb8bbdba2e3ab8a0e208ac
ManojKumar8973 <114487998+ManojKumar8973@users.noreply.github.com>
2022-12-02 10:43:27 +0530

    Add files via upload

---
+++ BankingProject/src/main/java/org/manoj/Main.java
@@ -0,0 +1,9 @@
+package org.manoj;
+
+public class Main {
+    public static void main(String[] args) {
+        Account ManojsAccount = new Account("344321",0,"Manoj","coolmanojk.2010@gmail.com","8973513809");
+        ManojsAccount.DepositMoney(100.0);
+        //ManojsAccount.WithdrawMoney(200.0);
+    }
+}
